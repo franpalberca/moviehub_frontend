4be5a519043e429b3be8d8f85a5e910ec98dcc97
Fran Pérez Alberca <fran.perez.alberca@gmail.com>
2023-09-05 14:25:24 +0200

    feat(main)receive fetch movies

--- src/api/GetMovies.tsx
+++ src/api/GetMovies.tsx
@@ -1,56 +1,77 @@
-import {useState, useEffect} from 'react';
-import {getDataApi} from './getDataApi';
-import {useAuth0} from '@auth0/auth0-react';
-import {Card} from '../components/card/Card';
+// import {useState, useEffect} from 'react';
+// import {getDataApi} from './getDataApi';
+// import {useAuth0} from '@auth0/auth0-react';
+// import {Card} from '../components/card/Card';
 import {useUserContext} from '../context/userContext';
 
-interface MoviesType {
-	id: string;
-	title: string;
-	score: number;
-	year: number;
-	country: string;
-	imageId: string;
-	genres: GenreType[];
-	genresArray: string[];
-	createdAt: string;
-	updatedAt: string;
-	usersId: string;
-}
+// interface MoviesType {
+// 	id: string;
+// 	title: string;
+// 	score: number;
+// 	year: number;
+// 	country: string;
+// 	imageId: string;
+// 	genres: GenreType[];
+// 	genresArray: string[];
+// 	createdAt: string;
+// 	updatedAt: string;
+// 	usersId: string;
+// }
 
-interface GenreType {
-	id: string;
-	genre: string;
-	createdAt: string;
-	updatedAt: string;
-	moviesId: string[];
-}
+// interface GenreType {
+// 	id: string;
+// 	genre: string;
+// 	createdAt: string;
+// 	updatedAt: string;
+// 	moviesId: string[];
+// }
 
-export const GetMovies = () => {
-	const [moviesData, setMoviesData] = useState<MoviesType[]>([]);
-	const userData = useUserContext();
-	const {getAccessTokenSilently} = useAuth0();
-	const url = `${import.meta.env.VITE_API_MOVIES}/${userData?.id}`;
-
-	useEffect(() => {
-		console.log("URL:", url);
-		const fetchMovies = async () => {
-			const data = await getDataApi(url, getAccessTokenSilently);
-			console.log("Movies Data:", data);
-			setMoviesData(data.movies);
-		};
-		fetchMovies();
-	}, []);
-
-	useEffect(() => {
-		console.log("Movies Data Updated:", moviesData); 
-	}, [moviesData]);
-
-	return (
-		<>
-			{moviesData.map((movie) => (
-				<Card key={movie.id} {...movie} />
-			))}
-		</>
-	);
+export const fetchMovies = async (url, getToken) => {
+	const token = await getToken();
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+        });
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Network response was not ok.");
+        }
+    } catch (error) {
+        console.log(error)
+    }
 };
+
+
+// export const GetMovies = () => {
+// 	const [moviesData, setMoviesData] = useState<MoviesType[]>([]);
+// 	const userData = useUserContext();
+// 	const {getAccessTokenSilently} = useAuth0();
+// 	const url = `${import.meta.env.VITE_API_MOVIES}/${userData?.id}`;
+
+// 	useEffect(() => {
+// 		console.log("URL:", url);
+// 		const fetchMovies = async () => {
+// 			const data = await getDataApi(url, getAccessTokenSilently);
+// 			console.log("Movies Data:", data);
+// 			setMoviesData(data.movies);
+// 		};
+// 		fetchMovies();
+// 	}, []);
+
+// 	useEffect(() => {
+// 		console.log("Movies Data Updated:", moviesData); 
+// 	}, [moviesData]);
+
+// 	return (
+// 		<>
+// 			{moviesData.map((movie) => (
+// 				<Card key={movie.id} {...movie} />
+// 			))}
+// 		</>
+// 	);
+// };
--- src/components/private/PrivateComponent.tsx
+++ src/components/private/PrivateComponent.tsx
@@ -1,23 +1,33 @@
 import {useEffect, useState} from 'react';
 import {useUserContext} from '../../context/userContext';
-import {GetMovies} from '../../api';
+import {fetchMovies} from '../../api';
+import {useAuth0} from '@auth0/auth0-react';
 
 export const PrivateComponent = () => {
+	const urlMovies = import.meta.env.VITE_API_MOVIES;
+	const {getAccessTokenSilently} = useAuth0();
 	const [movies, setMovies] = useState([]);
 	const userData = useUserContext();
-
+	let url = '';
+	if (userData) {
+		url = `${urlMovies}/${userData.id}`;
+	}
 	useEffect(() => {
-		async function fetchMovies() {
-			try {
-				const moviesData = await GetMovies();
-				setMovies(moviesData);
-			} catch (error) {
-				console.error('Error fetching movies:', error);
-			}
+		async function fetchDataMovies() {
+			const moviesData = await fetchMovies(url, getAccessTokenSilently);
+			console.log(moviesData);
+			setMovies(moviesData.movies);
+			// } catch (error) {
+			// 	console.error('Error fetching movies:', error);
+			// }
 		}
 
-		fetchMovies();
-	}, [userData]);
+		fetchDataMovies();
+	}, []);
+
+	useEffect(() => {
+		console.log(movies);
+	}, [movies]);
 
 	return (
 		<div>
@@ -25,8 +35,8 @@ export const PrivateComponent = () => {
 			<ul>
 				{movies.map((movie) => (
 					<li key={movie.id}>
-						<img src={movie.picture} alt={movie.name} />
-						{movie.name}
+						<img src={movie.imageUrl} alt={movie.title} />
+						{movie.title}
 					</li>
 				))}
 			</ul>
